458d9d3684dc16875d642742b871d131efd8897c
akash <akash.ju.msc@gmail.com>
2026-07-14 21:59:03 +0600

    feat(component): added basic input component

---
+++ src/app/components/input/input.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.css'
+})
+
+export class InputComponent {
+}
+
