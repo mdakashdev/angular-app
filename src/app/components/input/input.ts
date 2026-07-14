8bf40b446f0410a078627e7957c320d1f272dff6
akash <akash.ju.msc@gmail.com>
2026-07-14 22:02:48 +0600

    fix(component): add comment naming convention and vue equivalent angular

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -20,6 +20,31 @@ export class InputComponent {
 /**
  * vue er : <script setup></script>  equivalent - export class Input {}
  * vue e props: defineProps({label:String}) equivalent : @Input() label = '';
-
+ *
+ * Vue: Button.vue --> <Button />
+ * Angular : selector: 'app-input'  -> <app-input></app-input>
+ * Standard Naming Convention : file (user-card.ts), class (UserCardComponent), and Selector (app-user-card)
  */
 
+
+/* VUE
+<script setup lang="ts">
+  // props,reactive state,methods,computed,watch,lifecycle hooks
+  </script>
+  <template>
+  <!-- HTML --><!-- Components --><!-- Slots -->
+  </template>
+  <style scoped></style>
+ */
+
+/* Angular
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.css'
+})
+export class InputComponent {
+  // @Input(),state,methods,computed (Signals),lifecycle hooks
+}
+ */
