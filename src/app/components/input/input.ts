ce670066f20db38f7777746b554c2a9dca4ff67f
akash <akash.ju.msc@gmail.com>
2026-07-14 22:00:35 +0600

    fix(component): recive props in @Component

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -9,5 +9,17 @@ import { Component } from '@angular/core';
 })
 
 export class InputComponent {
+  @Input() label = '';
+  @Input() placeholder = '';
+  @Input() disabled = false;
+  @Input() readonly = false;
+  @Input() required = false;
+  @Input() type: 'text' | 'email' | 'number' = 'text';
 }
+
+/**
+ * vue er : <script setup></script>  equivalent - export class Input {}
+ * vue e props: defineProps({label:String}) equivalent : @Input() label = '';
+
+ */
 
